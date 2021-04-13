e34d5c23b7ee4cc4115936766359e553c27e892a
shtomar-adb <shtomar@adobe.com>
2021-04-13 01:21:11 -0700

    Added PublicApis.

---
+++ Campaign.swift
@@ -0,0 +1,69 @@
+/*
+ Copyright 2021 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License")
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import Foundation
+import AEPCore
+
+@objc(AEPCampaign)
+public class Campaign: NSObject, Extension {
+    
+    public var name = CampaignConstants.EXTENSION_NAME
+    public var friendlyName = CampaignConstants.FRIENDLY_NAME
+    public static var extensionVersion = CampaignConstants.EXTENSION_VERSION
+    public var metadata: [String : String]?
+    public let runtime: ExtensionRuntime
+    
+    public required init?(runtime: ExtensionRuntime) {
+        self.runtime = runtime
+        super.init()
+    }
+    
+    public func onRegistered() {
+        registerListener(type: EventType.campaign, source: EventSource.requestContent, listener: handleCampaignEvents)
+        registerListener(type: EventType.lifecycle, source: EventSource.responseContent, listener: handleLifecycleEvents)
+        registerListener(type: EventType.configuration, source: EventSource.responseContent, listener: handleConfigurationEvents)
+        registerListener(type: EventType.hub, source: EventSource.sharedState, listener: handleHubSharedStateEvent)
+        registerListener(type: EventType.genericData, source: EventSource.os, listener: handleGenericDataEvent)
+    }
+    
+    public func onUnregistered() {}
+    
+    public func readyForEvent(_ event: Event) -> Bool {
+        let configSharedStateStatus = getSharedState(extensionName: CampaignConstants.Configuration.EXTENSION_NAME, event: event)?.status ?? .none
+        return configSharedStateStatus == .set
+    }
+    
+    ///Handles events of type `Campaign`
+    private func handleCampaignEvents(event: Event){
+        
+    }
+    
+    ///Handles events of type `Lifecycle`
+    private func handleLifecycleEvents(event: Event){
+        
+    }
+    
+    ///Handles events of type `Configuration`
+    private func handleConfigurationEvents(event: Event){
+        
+    }
+    
+    ///Handles `Hub Shared state` events
+    private func handleHubSharedStateEvent(event: Event){
+        
+    }
+    
+    ///Handles `Generic Data` events
+    private func handleGenericDataEvent(event: Event){
+        
+    }
+}
