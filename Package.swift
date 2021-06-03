ed0c2ca952d44af146bfaab06ad27766c9f9e34a
Ryan Morales <rymorale@adobe.com>
2021-06-03 13:30:28 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         .library(name: "AEPCampaign", targets: ["AEPCampaign"])
     ],
     dependencies: [
-        .package(name: "AEPCore", url: "https://github.com/adobe/aepsdk-core-ios.git", from: "dev-v3.1.4"),
-        .package(name: "AEPRulesEngine", url: "https://github.com:adobe/aepsdk-rulesengine-ios.git", from: "dev-v1.0.2"),
+        .package(name: "AEPCore", url: "https://github.com/adobe/aepsdk-core-ios.git", .branch( "dev-v3.1.4")),
+        .package(name: "AEPRulesEngine", url: "https://github.com:adobe/aepsdk-rulesengine-ios.git", .branch("dev-v1.0.2")),
     ],
     targets: [
         .target(name: "AEPCampaign",
